fe121948ae1c6cbf187f6f875440de7b54414d06
dev-Shivam999 <shivamswami982@gmail.com>
2024-08-28 10:55:24 +0530

    time complexity

---
+++ array-qus/timeComplex/first.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    // time complexity  o(n)
+
+    // if   o(consent(1,2 ,50))  so the complexity is 1 for consent value if have a variable so the complexity ( in variable value )
+
+    // nested loops are have  complexity o(n*n)
+
+    //  a/b=loga-logb
+    // log(a*b)=loga+logb
+    // log(x^y)=ylogx n
+    
+    
+
+// iteration = 1 2 3 4 6 7  
+        // i= 1 3 5  7 9
+        // s =1 3 6 10 15
+    return 0;
+}
+
