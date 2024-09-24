ae393b2c359189e99dd17becce222584e662134d
dev-Shivam999 <shivamswami982@gmail.com>
2024-09-24 14:49:49 +0530

    add more

--- array/sort/selection/index.cpp
+++ array/sort/selection/index.cpp
@@ -3,14 +3,90 @@ using namespace std;
 
 int main()
 {
-
-    vector<int> arr = {1, , 3, 4, 5, 6, 2, 7, 9, 10, 8};
+    vector<int> arr = {1, 3, 2, 5,4,6,7,8,9,11};
     int sum = 0;
     for (int i = 0; i < arr.size(); i++)
     {
         sum += arr[i];
+        cout << arr[i] << " ";
+    }
+    cout << "\n"
+         << "sum = " << sum;
+
+    cout << "\n";
+
+    // int min = arr[0], max = arr[0];
+    // for (int i = 1; i < arr.size(); i++)
+    // {
+
+    //     if (min > arr[i])
+    //     {
+    //         min = arr[i];
+    //     }
+    //     if (max < arr[i])
+    //     {
+    //         max = arr[i];
+    //     }
+    // }
+    // cout << "\n"
+    //      << "min = " << min << " max = " << max;
+
+    for (int j = 0; j < arr.size(); j++)
+    {
+        int mini = arr[j];
+        int index = j;
+        for (int i = j + 1; i < arr.size(); i++)
+        {
+            if (arr[i] < mini)
+            {
+                mini = arr[i];
+                index = i;
+            }
+        }
+        swap(arr[j], arr[index]);
+      
+    }
+    cout << "\n";
+
+    for (auto i : arr)
+    {
+        cout << i << ' ';
     }
 
-    cout << sum << endl;
-    return 0;
-}
+    int n = arr[arr.size() - 1];
+    int num = n * (n + 1) / 2;
+
+    cout
+        << "\n"
+        << "missing : " << num-sum<<"\n";
+
+ 
+
+
+        vector<char> arr2 = {'A', 'B', 'D', 'E', 'F', 'H', 'I'};
+
+        for (int i = 0; i < arr2.size(); i++)
+        {
+            char max = arr2[i];
+            int index = i;
+
+            for (int j = i + 1; j < arr2.size(); j++)
+            {
+                if (max > arr2[j])
+                {
+                    max = arr2[j];
+                    index = j;
+                }
+            }
+
+            swap(arr2[i], arr2[index]);
+
+        }
+
+        for (auto it : arr2)
+        {
+            cout << it << " ";
+        }
+
+        return 0;
+    }
---
+++ array/sort/selection/tempCodeRunnerFile.cpp
@@ -0,0 +1,3 @@
+        // int temp = arr[j];
+        // arr[j] = arr[index];
+        // arr[index] = temp;
