2f6a81c70d667201b967c6697af1c11afdd75ffd
dev-Shivam999 <shivamswami982@gmail.com>
2024-09-25 14:41:31 +0530

    complete assignment

--- array-qus/assi/index.cpp
+++ array-qus/assi/index.cpp
@@ -1,66 +1,152 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int f(vector<int>&arr){
-    for(int i=0; i<arr.size();i++){
-        if (arr[i]==3)
+int f(vector<int> &arr)
+{
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (arr[i] == 3)
         {
             return i;
         }
-        
     }
     return -1;
 }
 
-void max(vector<int>&arr){
-    int max=arr[0];
-    int min=arr[0];
+void max(vector<int> &arr)
+{
+    int max = arr[0];
+    int min = arr[0];
     for (int i = 1; i < arr.size(); i++)
     {
-        if (arr[i] >max)
+        if (arr[i] > max)
         {
-            max=arr[i];
+            max = arr[i];
         }
-        if(min>arr[i]) min=arr[i];
+        if (min > arr[i])
+            min = arr[i];
     }
-    cout<<max<<" "<<min <<endl;
+    cout << max << " " << min << endl;
 }
 
-int l(vector<int>&arr){
-    int count=arr.size()+1;
+int l(vector<int> &arr)
+{
+    int count = arr.size() + 1;
     for (int i = 0; i < arr.size(); i++)
     {
-        if(arr[i]<8){
-            count=arr[i];
+        if (arr[i] < 8)
+        {
+            count = arr[i];
         }
-        
     }
-    if (count<arr.size())
+    if (count < arr.size())
     {
         return count;
-    }else{
-    return -1;
     }
+    else
+    {
+        return -1;
+    }
+}
+
+void co(vector<int> &arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (arr[i] == 3)
+        {
+            break;
+        }
+        count++;
+    }
+
+    cout << count;
+}
+
+vector<int> gre(vector<int> &arr)
+{
+    vector<int> a;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (arr[i] > 6)
+        {
+
+            a.push_back(arr[i]);
+        }
+    }
+    return a;
+}
+
+int re(vector<int> &arr)
+{
+    int ele = 11;
+    int count = 0;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (arr[i] == ele)
+        {
+            count++;
+        }
+    }
+    if (count == 0)
+        return -1;
+    else
+        return count;
+}
+
+void unique(vector<int> &arr)
+{
+
+    int count = 0;
+
+int sum=0;
+    for (int i = 0; i < arr.size(); i++)
+    {
+     sum+=arr[i];
+
+        for (int j = 0; j < arr.size(); j++)
+        {
+            if (i != j)
+            {
+
+                if (arr[i] == arr[j])
+                {
+                    count++;
+                }
+            }
+        }
+    }
+    
+
+    cout<<"sum of "<<sum <<endl;
+    if (count == 0) cout<<true;
+    else cout<<false;
+   
     
 }
 
-int main(){
+int main()
+{
 
-vector<int>arr={1,4,3,2,6,5,8,7,9,11};
+    vector<int> arr = {1, 4, 3, 2, 6, 5, 8, 7, 9, 11};
 
-// int a=f(arr);
+    // int a=f(arr);
 
-// max(arr);
+    // max(arr);
 
-// int b=l(arr);
-// cout<<b<<endl;
+    // int b=l(arr);
+    // cout<<b<<endl;
 
-1,2,3,4,5,6=15
+    // co(arr);
 
+    // vector<int>a=gre(arr);
 
-3,5,6=11
- 3
+    // int n = re(arr);
+    // cout<<n<<endl;
 
+    // unique(arr);
 
+    
 
-return 0;
+    return 0;
 }
--- array/sort/bubble/index.cpp
+++ array/sort/bubble/index.cpp
@@ -1,54 +1,73 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-
-
-// vector<int> f={1,2,3,5};
-// int count=f[0],fl=0;
-// for(int i=0;i<f.size();i++){
-// if(f[i]!=count){
-//     fl=1;
-//     break;
-// }
-// count++;
-
-// }
-// (fl!=0)?cout<<count:cout<<"-1";
-
-
-// vector<int>s={1,2,4,8,32};
-// int count=s[1]/s[0];
-// int f=0;
-// for (int i = 1; i <s.size(); i++)
-// {
-//     if (count!=s[i])
-//     {
-//         f=1;
-//         break;
-//     }
-//     count*=s[i];
-    
-// }
-// (f != 0) ? cout << count : cout << "-1";
-
-vector<int> s = {1, 3, 5, 7,11};
-int count=s[1]-s[0];
-int in=0;
-int f=0;
-for (int i = 1; i <s.size()-1; i++)
+int main()
 {
-    if (count!=s[i+1]-s[i])
-    {
-        count=count+s[i];
-        f=1;
-        break;
-    }
+
+    // vector<int> f={1,2,3,5};
+    // int count=f[0],fl=0;
+    // for(int i=0;i<f.size();i++){
+    // if(f[i]!=count){
+    //     fl=1;
+    //     break;
+    // }
+    // count++;
+
+    // }
+    // (fl!=0)?cout<<count:cout<<"-1";
+
+    // vector<int>s={1,2,4,8,32};
+    // int count=s[1]/s[0];
+    // int f=0;
+    // for (int i = 1; i <s.size(); i++)
+    // {
+    //     if (count!=s[i])
+    //     {
+    //         f=1;
+    //         break;
+    //     }
+    //     count*=s[i];
+
+    // }
+    // (f != 0) ? cout << count : cout << "-1";
+
+    // vector<int> s = {1, 3, 5, 7,11};
+    // int count=s[1]-s[0];
+    // int in=0;
+    // int f=0;
+    // for (int i = 1; i <s.size()-1; i++)
+    // {
+    //     if (count!=s[i+1]-s[i])
+    //     {
+    //         count=count+s[i];
+    //         f=1;
+    //         break;
+    //     }
+
+    //     count=s[i+1]-s[i];
+
+    // }
+    // (f != 0) ? cout << count : cout << "-1";
+
+    // vector<int> bubble = {21, 32, 12, 6, 54, 3, 4, 53};
     
-    count=s[i+1]-s[i];
+    // for (int i = 0; i < bubble.size(); i++)
+    // {
+    // for (int j = 0; j < bubble.size() -i- 1; j++)
+    // {
+    //     if (bubble[j+1] < bubble[j])
+    //     {
+    //         swap(bubble[j], bubble[j+1]);
+    //     }
+    // }
+    // }
 
-}
-(f != 0) ? cout << count : cout << "-1";
+    // for (auto i : bubble)
+    // {
+    //     cout << i << " ";
+    // }
 
-return 0;
+    
+
+    return 0;
 }
