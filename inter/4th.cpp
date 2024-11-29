74d3891f8c6305ca3e90cc41ae7d5c8e05ccf338
dev-Shivam999 <shivamswami982@gmail.com>
2024-11-29 10:11:53 +0530

    today

---
+++ inter/4th.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    // int arr[5]= { 10, 20, 30, 40, 50 };
+    // int zero[10]={0};
+    // for(auto it:zero){
+    //     cout<<it<<" ";
+    // }
+
+    // cout<<"give m size";
+    // int size;
+    // cin>>size;
+
+    // int sq[size];
+    // for (int i = 0; i < size; i++)
+    // {
+    //     sq[i]=i*i;
+    // }
+    // for( auto it:sq){
+    //     cout<<it<<" ";
+    // }
+
+    // int cp[] = {1, 2, 3, 4, 5};
+    // int size=sizeof(cp)/sizeof(cp[0]);
+    // int past[size];
+    // for( int i=0; i < size; i++){
+    // past[i]=cp[i];
+    // }
+
+    // for( auto it:past){
+    //     cout<<it<<" ";
+    // }
+
+    // vector<int>back;;
+
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     back.push_back(i + 1);
+    // }
+
+    // for (int it : back)
+    // {
+    //     cout << it << " ";
+    // }
+
+
+    // vector<int>em;
+
+    // if (em.empty())
+    // {
+    //     /* code */
+    //     cout<<"vector is empty"<<endl;
+    // }
+    // else{
+    //     em.pop_back()
+    // }
+
+    // vector<int> th = {10, 20, 30, 40, 50};
+
+    // th.at(3)=100;
+
+    // for(auto it:th){
+    //     cout<<it<<" ";
+    // }
+
+    // vector<int> tp = { 5,
+    //                    10,
+    //                    15,
+    //                    20 };
+                       
+    //                    for (int i = 0; i < tp.size(); i++)
+    //                    {
+    //                   cout<<tp[i]<<" ";
+    //                    }
+
+    // vector<int> cl = {1, 2, 3};
+    // cl.clear();
+
+    // if (cl.empty())
+    // {
+    //     /* code */
+    //     cout<<"o gaya ";
+    // }else{
+    //     cout<<"nhi hua";
+    // }
+    
+    return 0;
+}
