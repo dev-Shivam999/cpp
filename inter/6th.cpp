c87828668375031b4f5a9e33885720b39fee4fc7
dev-Shivam999 <shivamswami982@gmail.com>
2024-12-09 22:06:15 +0530

    complete today assignment

--- inter/6th.cpp
+++ inter/6th.cpp
@@ -4,25 +4,181 @@ using namespace std;
 int main()
 {
 
-//
+    //
 
-int arr[] = { 10,
-            20,
-            30,
-            40,
-            50 };
-            int p=3;
-            int size=sizeof(arr)/sizeof(arr[0]);
-            
-            for (int i =p ; i <size-1 ; i++)
-            {
-                arr[i] = arr[i+1];
-            }
-            
-            for (int i = 0; i <size-1; i++)
-            {
-                cout<<arr[i]<<" ";
-            }
-            
-             return 0;
+    // int arr[] = { 10,
+    //             20,
+    //             30,
+    //             40,
+    //             50 };
+    //             int p=3;
+    //             int size=sizeof(arr)/sizeof(arr[0]);
+
+    //             for (int i =p ; i <size-1 ; i++)
+    //             {
+    //                 arr[i] = arr[i+1];
+    //             }
+
+    //             for (int i = 0; i <size-1; i++)
+    //             {
+    //                 cout<<arr[i]<<" ";
+    //             }
+
+    // int Arr[] = {2, 4, 6, 8};
+    // int size = sizeof(Arr) / sizeof(Arr[0]);
+    // int NewArr[size + 1];
+    // int pos = 2, value = 3;
+    // int j = 0;
+    // for (int i = 0; i < size + 1; i++)
+    // {
+    //     if (i == pos)
+    //     {
+    //         NewArr[pos] = value;
+    //     }
+    //     else
+    //     {
+    //         NewArr[i] = Arr[j];
+    //         j++;
+    //     }
+    // }
+
+    // for (int i = 0; i < size + 1; i++)
+    // {
+    //     cout << NewArr[i] << " ";
+    // }
+
+    // int Pal[] = { 1,9 ,3, 3, 9, 1};
+    // bool Bool = false;
+    // int size = sizeof(Pal)/sizeof(Pal[0]);
+
+    // for (int i = 0; i <size/2 ; i++)
+    // {
+    // if (Pal[i] !=Pal[size-1-i])
+    // {
+    //     Bool=true;
+    //     break;
+    // }
+
+    // }
+
+    // if (Bool){
+    //     cout<<"no";
+    // }else{
+    //     cout<<"ha";
+    // }
+
+    //     int arr[] = {1,
+    //                  2,
+    //                  3,
+    //                  4,
+    //                  5};
+    //     int f = arr[0];
+    //     int size=sizeof(arr)/sizeof(arr[0]);
+    //     for (int i = 0; i <size-1 ; i++)
+    //     {
+    //       arr[i] =arr[i+1];
+    //     }
+    //     arr[size-1] =f;
+
+    // for (int i = 0; i < size; i++)
+    // {
+    //     cout<<arr[i]<<" ";
+    // }
+
+    //         int arr[] = {1,
+    //                      2,
+    //                      3,
+    //                      4,
+    //                      5};
+
+    // bool found=false ;
+
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     if (arr[i] ==10)
+    //     {
+    //         found = true;
+    //     }
+
+    // }
+
+    // if (found){
+    //      cout<<"found";
+    // }else{
+    //     cout<<"not found";
+    // }
+
+    //     int arr[] = { 1,
+    //                                        2,
+    //                                        3,
+    //                                        4,
+    //                                        5};
+    // int e=0,o=0;
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     if (arr[i]%2 ==0)
+    //     {
+    //         e++;
+    //     }else{
+    //         o++;
+    //     }
+
+    // }
+
+    // cout<<e<<" "<<o<<endl;
+    // int arr[] = {1,
+    //              2,
+    //              3,
+    //              4,
+    //              5};
+
+    //              int sum=0,avg=0;
+
+    //              for (int i = 0; i <5; i++)
+    //              {
+    //                 sum+=arr[i];
+
+    //              }
+    //              avg=sum/5;
+
+    //              cout<<sum<<" "<<avg<<endl;
+
+    //     int arr[] = { 1,
+    //                                        2,
+    //                                        3,
+    //                                        4,
+    //                                        5};
+    // for (int i = 0; i < 2; i++)
+    // {
+    //     int temp=arr[i];
+    //     arr[i]=arr[5-1-i];
+    //     arr[5-1-i]=temp;
+
+    // }
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     /* code */
+    //     cout<<arr[i]<<" "<<endl;
+    // }
+//     int arr[] = { 1,
+//                                        2,
+//                                        3,
+//                                        4,
+//                                        5};
+
+// int min = arr[0];
+// int max = arr[0];
+
+// for (int i = 0; i < 5; i++)
+// {
+//     if (arr[i] < min){
+//         min = arr[i];
+//     }else if (arr[i] > max){
+//          max=arr[i];
+//     }
+// }
+
+// cout<<min<<" "<<max<<endl;
+
+                  return 0;
 }
