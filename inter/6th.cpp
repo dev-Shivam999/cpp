51115e8e3bfea27cf09dae7d182eb94804a5b923
dev-Shivam999 <shivamswami982@gmail.com>
2024-12-09 10:38:55 +0530

    verify

--- inter/6th.cpp
+++ inter/6th.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int main()
 {
 
-    vector<int> A = {1, 3, 5};
+    vector<int> A = {1, 3, 5,8,9};
 
-    vector<int> B = {2, 4, 6};
+    vector<int> B = {2, 4, 6,7};
     vector<int> C;
     int j = 0;
     int i = 0;
-    while (i < A.size())
+    while (i < A.size()&& j < B.size())
     {
         if (A[i] < B[j])
         {
@@ -22,6 +22,14 @@ int main()
             C.push_back(B[j]);
             j++;
         }
+    }
+    while (j < B.size()){
+C.push_back(B[j]);
+        j++;
+    }
+    while (i < A.size()){
+C.push_back(A[i]);
+        i++;
     }
 
     for (auto it : C)
