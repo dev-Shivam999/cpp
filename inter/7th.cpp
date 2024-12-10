74f9d15af327e1836d38e3446efa77cb2eb6d0e1
dev-Shivam999 <shivamswami982@gmail.com>
2024-12-10 12:55:52 +0530

    ok

--- inter/7th.cpp
+++ inter/7th.cpp
@@ -1,7 +1,134 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-int main(){
+int main()
+{
+    // vector<int> arr1 = {1, 3, 5, 7};
+    // vector<int> arr2 = {2, 4, 6, 8};
+    // int i = 0, j = 0;
+    // vector<int> newarr;
 
-return 0;
+    // while (i < arr1.size()/2 && j < arr2.size()/2)
+    // {
+    //     if (arr1[i] < arr2[j])
+    //     {
+    //         newarr.push_back(arr1[i]);
+    //         i++;
+    //     }
+    //     else
+    //     {
+    //         newarr.push_back(arr2[j]);
+    //         j++;
+    //     }
+    // }
+
+    // while (i < arr1.size()/2)
+    // {
+    //     newarr.push_back(arr1[i]);
+    //     i++;
+    // }
+
+    // while (j < arr2.size()/2)
+    // {
+    //     newarr.push_back(arr2[j]);
+    //     j++;
+    // }
+
+    // for (auto it : newarr)
+    // {
+    //     cout << it << " ";
+    // }
+
+    // vector<int> pal = {121,
+    //                    232,
+    //                    345,
+    //                    454,
+    //                    1221,
+    //                    345};
+
+    // vector<string> ans;
+    // for (int i = 0; i < pal.size(); i++)
+    // {
+    //     int temp = pal[i], rem = 0;
+
+    //     while (temp != 0)
+    //     {
+
+    //          rem = rem * 10 + temp % 10;
+    //          temp /= 10;
+    //     }
+
+    //     if (rem == pal[i])
+    //     {
+    //         ans.push_back("yes");
+    //     }
+    //     else
+    //     {
+    //         ans.push_back("no");
+    //     }
+    // }
+    // for (auto it : ans)
+    // {
+    //     cout << it << " ";
+    // }
+    // vector<int> arr = {0, 5, 0, 3, 4, 0, 1};
+    // int n = arr.size();
+    // int index = 0;
+
+    // for (int i = 0; i < n; ++i)
+    // {
+    //     if (arr[i] != 0)
+    //     {
+    //         arr[index++] = arr[i];
+    //     }
+    // }
+
+    // while (index < n)
+    // {
+    //     arr[index++] = 0;
+    // }
+
+    // for(auto  it:arr){
+    //     cout<<it<<" ";
+    // }
+
+    // vector<int> cir = {1,
+    //                    2,
+    //                    3,
+    //                    4};
+
+    // int mul = 1;
+    // for (auto it : cir)
+    // {
+    //     mul *= it;
+    // }
+    // for ( int i = 0; i < cir.size(); i++)
+    // {
+    //      cir[i]=mul/cir[i];
+    // }
+    // for(auto it :cir){
+    //     cout << it << " ";
+    // }
+
+    // vector<int> Mis = {3,
+    //                    4,
+    //                    -1,
+    //                    1};  
+
+    // int num = 0, max = Mis[0];
+    // for (int i = 0; i < Mis.size(); i++)
+    // {
+    //     if (Mis[i] > 0)
+    //     {
+    //         num += Mis[i];
+    //     }
+    //     if (max < Mis[i])
+    //         max = Mis[i];
+    // }
+ 
+
+    // cout <<  ((max/2) * (max + 1))-num << endl;
+
+    return 0;
 }
