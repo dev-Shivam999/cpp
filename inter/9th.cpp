2ece6aca8edd1a07d051a609a116ce7ff5f8b9c9
dev-Shivam999 <shivamswami982@gmail.com>
2024-12-12 17:49:31 +0530

    9th

---
+++ inter/9th.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    // vector<int> Dup = {3, 5, 2, 4, 5, 2};
+    // int num = -1;
+    // for (int i = 0; i < Dup.size(); i++)
+    // {
+    //     for (int j = 0; j < Dup.size(); j++)
+    //     {
+    //         if (i != j)
+    //         {
+    //             if (Dup[i] == Dup[j])
+    //             {
+    //                 num = Dup[i];
+    //                 break;
+    //             };
+    //         }
+    //         if (num != -1)
+    //         {
+    //             break;
+    //         }
+    //     }
+    // }
+    // cout << num << endl;
+
+    // vector<int> Count = {1, 2, 2, 3, 4, 4, 5};
+    // vector<int> NewAr;
+
+    // int count = 0;
+    // for (int i = 0; i < Count.size(); i++)
+    // {
+    //      bool Flag = false;
+    //     for (int j = 0; j < Count.size(); j++)
+    //     {
+    //         if (i != j)
+    //         {
+    //             if (Count[i] == Count[j])
+    //             {
+    //                 Flag=true;
+    //                 break;
+
+    //             }
+    //         }
+    //     }
+    //     if (!Flag){
+    //         NewAr.push_back(Count[i]);
+    //         count++;
+    //     }
+    // }
+    // cout<<count<<endl;
+
+    // for (int i = 0; i < NewAr.size(); i++)
+    // {
+    //     cout<<NewAr[i]<<endl;
+
+    // }
+
+    //     vector<int> FindDub = {1, 2, 3, 4, 4, 5, 5, 5, 6, 6, 1, 2, 2};
+    //     vector<int>Res(FindDub.size(),0);
+
+    //  for (int i = 0; i <Res.size(); i++)
+    //  {
+    //   Res[FindDub[i]]++;
+    //  }
+
+    //  for (int i = 0; i <Res.size(); i++)
+    //  {
+    //     if (Res[i]>1)
+    //     {cout<<i<<" ";
+    //     }
+
+    //  }
+
+
+
+
+
+
+    return 0;
+}
