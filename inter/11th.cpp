a55ede7226a78fb5ce6d7d46fe615c7de34b0e56
dev-Shivam999 <shivamswami982@gmail.com>
2024-12-18 10:28:35 +0530

    11th

---
+++ inter/11th.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main(){
+int arr[5]={1,2,3,4,5};
+int arr2[6]={10,20,30};
+
+// char arr3[]="Hello";
+
+// for (int i = 0; i <5; i++)
+// {
+//     cout<<arr3[i]<<" ";
+// }
+
+int n=10;
+int arr4[10];
+
+for (int i = 1; i <= 10; i++)
+{
+    arr4[i]=i*i;
+}
+
+
+int n2=5;
+
+int arr5[n2];
+
+for (int i = 1; i <= n2; i++)
+{
+arr5[i]=i*5;
+}
+
+int arr6[5] = {-1, -1, -1, -1, -1};
+
+int arr7[] = {1, 2, 3, 4, 5}; 
+int arr8[5];                  
+
+
+for (int i = 0; i < 5; i++)
+{
+    arr8[i] = arr7[i];
+}
+
+
+
+
+
+return 0;
+}
