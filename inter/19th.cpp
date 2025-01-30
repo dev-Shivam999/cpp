fd4e8fe906b39344370a19943c47ad148b80958d
dev-Shivam999 <shivamswami982@gmail.com>
2025-01-30 10:32:21 +0530

    add initial implementation of a C++ program with a vector of pairs

---
+++ inter/19th.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct p{
+    int data;
+    string lol;
+};
+int main(){
+    vector<pair<int,string>> v = {{1,"lol"},{2,"lol"},{3,"lol"}};
+    for(auto it : v){
+        cout<<it.first<<" "<<it.second<<endl;
+    }
+
+return 0;
+}
